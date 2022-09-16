855cf3df6d1bb6fdc8e1ace9b7b31e3612291b51
riko <54302736+nasi-goreng@users.noreply.github.com>
2022-09-16 10:40:01 +0900

    committing before pull

---
+++ components/LoginIos.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
+import "expo-dev-client";
+import auth from "@react-native-firebase/auth";
+
+function LoginIos() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+  GoogleSignin.configure({
+    webClientId:
+      "842723940344-jrjtrlkbl0t5uosmvjuajgi4a218puv9.apps.googleusercontent.com",
+  });
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  // なぜか関数式に直した
+  const onGoogleButtonPress = async () => {
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    // return auth().signInWithCredential(googleCredential);
+    const user_sign_in = auth().signInWithCredential(googleCredential);
+    user_sign_in
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  if (initializing) return null;
+
+  if (!user) {
+    return (
+      <View style={StyleSheet.container}>
+        <GoogleSigninButton
+          style={{ width: 300, height: 65, marginTop: 300 }}
+          onPress={onGoogleButtonPress}
+        />
+      </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <View style={{marginTop:100, alignItems:'center'}}>
+        <Text style={styles.text}>Welcome, {user.displayName}</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 23,
+  }
+})
+
+export default LoginIos;
