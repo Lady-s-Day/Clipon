557e9b3bc5c4294a15daaaf68a15f6de2724a313
Ayumi Watanabe <i.anzu23@gmail.com>
2022-09-26 15:03:54 +0900

    fixed gitignore

--- components/Camera.js
+++ components/Camera.js
@@ -8,11 +8,13 @@ import {
   View,
   Image,
 } from "react-native";
+import Environment from "../config/environments";
 
 const CameraComponent = () => {
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [camera, setCamera] = useState(null);
   const [picture, setPicture] = useState();
+  const [string, setString] = useState("");
 
   useEffect(() => {
     if (picture) {
@@ -48,43 +50,77 @@ const CameraComponent = () => {
 
   const takePicture = async () => {
     if (camera) {
-      const image = await camera.takePictureAsync();
+      const image = await camera.takePictureAsync({
+        base64: true,
+      });
       console.log("写真撮ったよ");
       // console.log("image.uri::::::", image.uri);
       setPicture(image.uri);
+      sendCloudVision(image.base64);
     }
+  };
+
+  const sendCloudVision = async (image) => {
+    const body = JSON.stringify({
+      requests: [
+        {
+          features: [{ type: "TEXT_DETECTION", maxResults: 1 }],
+          image: {
+            content: image,
+          },
+        },
+      ],
+    });
+    const response = await fetch(
+      "https://vision.googleapis.com/v1/images:annotate?key=" +
+        Environment["GOOGLE_CLOUD_VISION_API_KEY"],
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: body,
+      }
+    );
+    const resJson = await response.json();
+    console.log(resJson);
+
+    setString({
+      ocrText: resJson.responses[0].textAnnotations[0].description,
+    });
   };
 
   // =================================================================
 
-  function cloudVision() {
-    function main() {
-      // [START vision_quickstart]
-      async function quickstart() {
-        // Imports the Google Cloud client library
-        const vision = require("@google-cloud/vision");
+  // function cloudVision() {
+  //   function main() {
+  //     // [START vision_quickstart]
+  //     async function quickstart() {
+  //       // Imports the Google Cloud client library
+  //       const vision = require("@google-cloud/vision");
 
-        // Creates a client
-        const client = new vision.ImageAnnotatorClient();
+  //       // Creates a client
+  //       const client = new vision.ImageAnnotatorClient();
 
-        // Performs label detection on the image file
-        const [result] = await client.documentTextDetection(picture);
-        const labels = result.fullTextAnnotation.text;
-        // console.log("Labels:");
-        console.log(labels);
-        // labels.forEach((label) => console.log(label.description));
-      }
-      quickstart();
-      // [END vision_quickstart]
-    }
+  //       // Performs label detection on the image file
+  //       const [result] = await client.documentTextDetection(picture);
+  //       const labels = result.fullTextAnnotation.text;
+  //       // console.log("Labels:");
+  //       console.log(labels);
+  //       // labels.forEach((label) => console.log(label.description));
+  //     }
+  //     quickstart();
+  //     // [END vision_quickstart]
+  //   }
 
-    process.on("unhandledRejection", (err) => {
-      console.error(err.message);
-      process.exitCode = 1;
-    });
+  //   process.on("unhandledRejection", (err) => {
+  //     console.error(err.message);
+  //     process.exitCode = 1;
+  //   });
 
-    main(...process.argv.slice(2));
-  }
+  //   main(...process.argv.slice(2));
+  // }
 
   // ================================================================
 
