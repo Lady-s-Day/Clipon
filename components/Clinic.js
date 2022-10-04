7eef5d650dc0836afa1e95f9e6a238a9bf0c0e31
riko <54302736+nasi-goreng@users.noreply.github.com>
2022-10-04 11:27:26 +0900

    fixed font in clinic.js

--- components/Clinic.js
+++ components/Clinic.js
@@ -66,168 +66,193 @@ function Clinic({ route, navigation }) {
           obj[clinic.id] = true;
         }
         setFavorite(obj);
-        console.log(obj)
+        console.log(obj);
       } catch (err) {
         console.error("Error getting saved", err);
       }
     })();
   }, []);
 
-
-
   return (
     <View style={{ flex: 1, backgroundColor: Colors.light }}>
-      {selectedClinic &&
+      {selectedClinic && (
         <>
-        <Image
-          style={{
-            width: "100%",
-            height: 100,
-            backgroundColor: Colors.light,
-          }}
-          resizeMode="cover"
-          source={{ uri: selectedClinic.image }}
-        />
-        <View style={{ flex: 1, flexDirection: "row", paddingTop: 15, paddingLeft: 15, paddingRight: 10 }}>
-          <View style={{ flex: 3 }}>
-            <Text
-              style={{
-                fontFamily: "font2bold"
-                fontSize: 22,
-                color: Colors.navy,
-              }}
-            >
-              {selectedClinic.clinic_name}
-            </Text>
-          </View>
-          <View style={{ flex: 1, paddingLeft: 15, paddingRight: 10 }}>
-            {favorite[id] ? (
-              <Icon
-                size={30}
-                name="favorite"
-                color={Colors.red}
-                onPress={() =>
-                  toggleFavorite(id, favorite, setFavorite, user)
-                }
-              />
-            ) : (
-              <Icon
-                size={30}
-                name="favorite-outline"
-                color={Colors.red}
-                onPress={() =>
-                  toggleFavorite(id, favorite, setFavorite, user)
-                }
-              />
-            )}
-          </View>
-        </View>
-        <View style={{ flex: 16, padding: 10 }}>
+          <Image
+            style={{
+              width: "100%",
+              height: 100,
+              backgroundColor: Colors.light,
+            }}
+            resizeMode="cover"
+            source={{ uri: selectedClinic.image }}
+          />
           <View
             style={{
+              flex: 1,
               flexDirection: "row",
-              justifyContent: "flex-start",
-              flexWrap: "wrap"
+              paddingTop: 15,
+              paddingLeft: 15,
+              paddingRight: 10,
             }}
           >
-            {treatments[id]?.map((type, i) => {
-              return (
-                <View key={i} style={{
-                  alignSelf: "flex-start",
-                  marginRight: 5,
-                  marginBottom: 5
-                }}>
-                  <Chip
-                    onPress={() => console.log("Pressed")}
-                    style={{
-                      backgroundColor: Colors.white,
-                      size: "small",
-                      borderColor: Colors.red,
-                      borderWidth: 1,
-                    }}
-                    textStyle={{
-                      fontSize: 10,
-                      includeFontPadding: false,
-                      textAlign: "center",
-                      textAlignVertical: "center",
-                    }}
-                    compact
-                  >
-                    {type}
-                  </Chip>
-                </View>
-              );
-            })}
-          </View>
-          <View style={{ padding: 5 }}>
-            <Text style={{ color: Colors.navy, fontWeight: "bold" }}>HP:</Text>
-            <Hyperlink
-              linkStyle={{ color: Colors.blue }}
-              onPress={(url, text) => {
-                Linking.canOpenURL(url).then((supported) => {
-                  if (!supported) {
-                    console.log("無効なURLです: " + url);
-                  } else {
-                    return Linking.openURL(url);
-                  }
-                });
-              }}
-            >
-              <Text style={{ fontSize: 15 }}>{selectedClinic.url}</Text>
-            </Hyperlink>
-          </View>
-          <Button
-            radius={5}
-            style={{ marginTop: 15, marginBottom: 10 }}
-            color={Colors.red}
-            onPress={() =>
-              navigation.navigate("CreateReview", {
-                id: id,
-              })
-            }
-          >レビューを投稿する</Button>
-          <ScrollView style={{ backgroundColor: Colors.light }}>
-            {reviews?.map((review, index) => {
-            return (
-              <Card
-                key={index}
-                containerStyle={{
-                  borderColor: Colors.brown,
-                  borderWidth: 0.5,
-                  borderRadius: 8,
+            <View style={{ flex: 3 }}>
+              <Text
+                style={{
+                  fontFamily: "font2bold",
+                  fontSize: 22,
+                  color: Colors.navy,
                 }}
               >
-                <Text style={styles.text}>{review.text}</Text>
-                <View style={{ marginTop: 5 }}>
-                  <Text style={{fontFamily: "font2"}}>
-                    {review.user_name}
-                    {review.approved && (
-                      <Badge status="error" value={"Approved"} />
-                    )}
-                  </Text>
-                </View>
-                <Text style={{fontFamily: "font2"}}>{format(parseISO(review.date), formatDate)}</Text>
-              </Card>
-            );
-          })}
-          </ScrollView>
-        </View>
-      </>
-      }
-    </View >
+                {selectedClinic.clinic_name}
+              </Text>
+            </View>
+            <View style={{ flex: 1, paddingLeft: 15, paddingRight: 10 }}>
+              {favorite[id] ? (
+                <Icon
+                  size={30}
+                  name="favorite"
+                  color={Colors.red}
+                  onPress={() =>
+                    toggleFavorite(id, favorite, setFavorite, user)
+                  }
+                />
+              ) : (
+                <Icon
+                  size={30}
+                  name="favorite-outline"
+                  color={Colors.red}
+                  onPress={() =>
+                    toggleFavorite(id, favorite, setFavorite, user)
+                  }
+                />
+              )}
+            </View>
+          </View>
+          <View style={{ flex: 16, padding: 10 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                flexWrap: "wrap",
+              }}
+            >
+              {treatments[id]?.map((type, i) => {
+                return (
+                  <View
+                    key={i}
+                    style={{
+                      alignSelf: "flex-start",
+                      marginRight: 5,
+                      marginBottom: 5,
+                    }}
+                  >
+                    <Chip
+                      onPress={() => console.log("Pressed")}
+                      style={{
+                        backgroundColor: Colors.white,
+                        size: "small",
+                        borderColor: Colors.red,
+                        borderWidth: 1,
+                      }}
+                      textStyle={{
+                        fontSize: 10,
+                        includeFontPadding: false,
+                        textAlign: "center",
+                        textAlignVertical: "center",
+                        fontFamily: "font2",
+                      }}
+                      compact
+                    >
+                      {type}
+                    </Chip>
+                  </View>
+                );
+              })}
+            </View>
+            <View style={{ padding: 5 }}>
+              <Text style={{ color: Colors.navy, fontFamily: "font2" }}>
+                HP:
+              </Text>
+              <Hyperlink
+                linkStyle={{ color: Colors.blue, fontFamily: "font2" }}
+                onPress={(url, text) => {
+                  Linking.canOpenURL(url).then((supported) => {
+                    if (!supported) {
+                      console.log("無効なURLです: " + url);
+                    } else {
+                      return Linking.openURL(url);
+                    }
+                  });
+                }}
+              >
+                <Text style={{ fontSize: 15 }}>{selectedClinic.url}</Text>
+              </Hyperlink>
+            </View>
+            <Button
+              radius={5}
+              style={{ marginTop: 15, marginBottom: 10 }}
+              color={Colors.red}
+              onPress={() =>
+                navigation.navigate("CreateReview", {
+                  id: id,
+                })
+              }
+            >
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: Colors.white,
+                  fontFamily: "font2bold",
+                  padding: 1,
+                }}
+              >
+                レビューを投稿する
+              </Text>
+            </Button>
+            <ScrollView style={{ backgroundColor: Colors.light }}>
+              {reviews?.map((review, index) => {
+                return (
+                  <Card
+                    key={index}
+                    containerStyle={{
+                      borderColor: Colors.brown,
+                      borderWidth: 0.5,
+                      borderRadius: 8,
+                    }}
+                  >
+                    <Text style={styles.text}>{review.text}</Text>
+                    <View style={{ marginTop: 5 }}>
+                      <Text style={{ fontFamily: "font2" }}>
+                        {review.user_name}
+                        {review.approved && (
+                          <Badge status="error" value={"Approved"} />
+                        )}
+                      </Text>
+                    </View>
+                    <Text style={{ fontFamily: "font2" }}>
+                      {format(parseISO(review.date), formatDate)}
+                    </Text>
+                  </Card>
+                );
+              })}
+            </ScrollView>
+          </View>
+        </>
+      )}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     padding: 20,
-    backgroundColor: Colors.white
+    backgroundColor: Colors.white,
   },
   text: {
     fontSize: 14,
     marginBottom: 10,
     color: Colors.navy,
-    fontFamily: "font2bold"
+    fontFamily: "font2bold",
   },
 });
 
